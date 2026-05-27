54e0c4218bf3f5d51520b8d8d35c56212eb0d1e7
soham-chitnis10 <sohamchitnis10@gmail.com>
2026-05-27 16:34:55 -0400

    update trial gen and envs

--- stable_worldmodel/envs/__init__.py
+++ stable_worldmodel/envs/__init__.py
@@ -31,35 +31,11 @@ register(
     entry_point='stable_worldmodel.envs.simple_point_maze:SimplePointMazeEnv',
 )
 
-# register(
-#     id='swm/maze2d-umaze-v1',
-#     entry_point='stable_worldmodel.envs.diverse_maze.env:DiverseMazeEnv',
-#     kwargs={'env_name': 'maze2d-umaze-v1'},
-# )
-
-# register(
-#     id='swm/maze2d-medium-v1',
-#     entry_point='stable_worldmodel.envs.diverse_maze.env:DiverseMazeEnv',
-#     kwargs={'env_name': 'maze2d-medium-v1'},
-# )
-
-# register(
-#     id='swm/maze2d-large-v1',
-#     entry_point='stable_worldmodel.envs.diverse_maze.env:DiverseMazeEnv',
-#     kwargs={'env_name': 'maze2d-large-v1'},
-# )
-
 register(
     id='swm/maze2d_small_diverse',
     entry_point='stable_worldmodel.envs.diverse_maze.env:DiverseMazeEnv',
     kwargs={'env_name': 'maze2d_small_diverse'},
 )
-
-# register(
-#     id='swm/maze2d_small_diverse_single',
-#     entry_point='stable_worldmodel.envs.diverse_maze.env:DiverseMazeEnv',
-#     kwargs={'env_name': 'maze2d_small_diverse_single'},
-# )
 
 register(
     id='swm/maze2d_medium_diverse',
@@ -72,12 +48,6 @@ register(
     entry_point='stable_worldmodel.envs.diverse_maze.env:DiverseMazeEnv',
     kwargs={'env_name': 'maze2d_large_diverse'},
 )
-
-# register(
-#     id='swm/ant_medium_diverse',
-#     entry_point='stable_worldmodel.envs.diverse_maze.env:DiverseMazeEnv',
-#     kwargs={'env_name': 'ant_medium_diverse'},
-# )
 
 register(
     id='swm/TwoRoom-v1',
